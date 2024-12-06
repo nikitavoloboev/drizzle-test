2b6ede86a7bbc799138a24057cb07b095c1b4b67
Nikita <github@nikiv.dev>
2024-12-07 00:52:11 +0100

    .

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,3 +1,22 @@
-async function main() {}
+// as per https://orm.drizzle.team/docs/connect-bun-sqlite
+import { drizzle } from "drizzle-orm/bun-sqlite"
+import { lastClonesWindow } from "../src/schema"
+
+async function main() {
+  const db = drizzle(process.env.DB_FILE_NAME!)
+
+  // await clearLastClonesWindowTable()
+  // await db.insert(lastClonesWindow).values({
+  //   name: "main.ts -- drizzle",
+  // })
+
+  // const result = await db.select().from(lastClonesWindow)
+  // console.log(result)
+}
+
+async function clearLastClonesWindowTable() {
+  const db = drizzle(process.env.DB_FILE_NAME!)
+  await db.delete(lastClonesWindow)
+}
 
 main()
